b5db291a259f53032a882c1d9cfc1e50d3bdf77d
alex <alex@mbpro13.local>
2020-03-24 18:00:13 -0400

    new demo_repo_created

---
+++ grp1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('fruits.csv')
+
+tbl = df.groupby(['Fruit', 'Name'], as_index=False).sum().pivot('Fruit', 'Name', 'Number').fillna(0)
+
+print(tbl)
+
+tbl.head(20).plot(kind='bar', figsize=(10, 5))
+plt.title('Fruits By Name')
+plt.ylabel('Consumed Fruits by Person')
+plt.xlabel('Number of Fruits')
+
+
+plt.show()
+print(df)
+# print(df.head(5))
